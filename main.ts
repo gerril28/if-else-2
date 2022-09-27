e4542c4eb02566e177a0ba8fd4285c60856c3329
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-27 09:25:28 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    if (age == 16 && (written_test && g1)) {
+        basic.showString("License YES SIR")
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+let written_test = false
+let g1 = false
+let age = 0
+age = 16
+g1 = true
+written_test = true
